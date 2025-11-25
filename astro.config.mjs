f114c7d73bac0cd5156cd29b83f7b57b9f3cd459
Technova24x7 <technova24x7@gmail.com>
2025-11-25 18:23:10 +0530

    config update for routes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site:"https://technova24x7.github.io/universaladv/",
+  base:'/',
   vite: {
     plugins: [tailwindcss()],
   },
